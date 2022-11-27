f92d40b410697ce376d957b9bbb8a7585399e33f
1Exit <90748939+1Exit@users.noreply.github.com>
2022-11-27 19:08:12 +0300

    Add files via upload

---
+++ qb-menu/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Menu'
+version '0.0.1'
+
+client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/*.mp3',
+    'html/script.js',
+    'html/style.css'
+}
+
+lua54 'yes'
